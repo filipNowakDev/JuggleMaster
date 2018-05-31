a198a538f1d837395dba7ba7c5114423572da96b
jankanapka <filiposn@gmail.com>
2018-05-31 16:20:42 +0200

    Added dummy gameplay screen

---
+++ core/src/com/filip/jugglemaster/screens/GameplayScreen.java
@@ -0,0 +1,15 @@
+package com.filip.jugglemaster.screens;
+
+import com.filip.jugglemaster.JuggleMasterGame;
+
+public class GameplayScreen extends AbstractScreen
+{
+	public GameplayScreen(JuggleMasterGame game)
+	{
+		super(game);
+		init();
+	}
+
+	private void init()
+	{}
+}
