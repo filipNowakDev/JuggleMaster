683a08f2b536d952d68d824ecc1a227bd49ae777
jankanapka <filiposn@gmail.com>
2018-05-31 15:27:54 +0200

    init develop

--- core/src/com/filip/jugglemaster/JuggleMasterGame.java
+++ core/src/com/filip/jugglemaster/JuggleMasterGame.java
@@ -1,32 +1,40 @@
 package com.filip.jugglemaster;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class JuggleMasterGame extends ApplicationAdapter {
+public class JuggleMasterGame extends Game
+{
+	public static final String NAME = "Juggle Master";
+	public static final int WIDTH = 480;
+	public static final int HEIGHT = 700;
+
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
-	public void create () {
+	public void create()
+	{
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
-	public void render () {
+	public void render()
+	{
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose()
+	{
 		batch.dispose();
 		img.dispose();
 	}
--- desktop/src/com/filip/jugglemaster/desktop/DesktopLauncher.java
+++ desktop/src/com/filip/jugglemaster/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.filip.jugglemaster.JuggleMasterGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = JuggleMasterGame.NAME;
+		config.width = JuggleMasterGame.WIDTH;
+		config.height = JuggleMasterGame.HEIGHT;
+		config.resizable = false;
 		new LwjglApplication(new JuggleMasterGame(), config);
 	}
 }
