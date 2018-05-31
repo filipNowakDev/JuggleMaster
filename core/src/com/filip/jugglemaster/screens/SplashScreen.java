f64fe9591de181dd54884ef8644210d86dc133f5
jankanapka <filiposn@gmail.com>
2018-05-31 16:28:11 +0200

    Changing splash to game screen

--- core/src/com/filip/jugglemaster/screens/SplashScreen.java
+++ core/src/com/filip/jugglemaster/screens/SplashScreen.java
@@ -1,16 +1,24 @@
 package com.filip.jugglemaster.screens;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Timer;
 import com.filip.jugglemaster.JuggleMasterGame;
 
 public class SplashScreen extends AbstractScreen
 {
 	private Texture splashImage;
 
-	public SplashScreen(JuggleMasterGame game)
+	public SplashScreen(final JuggleMasterGame game)
 	{
 		super(game);
 		init();
+		Timer.schedule(new Timer.Task(){
+			@Override
+			public void run()
+			{
+				game.setScreen(new GameplayScreen(game));
+			}
+		}, 1);
 	}
 
 	private void init()
