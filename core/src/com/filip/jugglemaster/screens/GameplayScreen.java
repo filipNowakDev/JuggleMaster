d23e93e2664b9cb143c017330bd1f9804d110fe5
jankanapka <filiposn@gmail.com>
2018-05-31 17:41:37 +0200

    Added ball button

--- core/src/com/filip/jugglemaster/screens/GameplayScreen.java
+++ core/src/com/filip/jugglemaster/screens/GameplayScreen.java
@@ -1,11 +1,16 @@
 package com.filip.jugglemaster.screens;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.reflect.Field;
 import com.filip.jugglemaster.JuggleMasterGame;
 import com.filip.jugglemaster.entities.Ball;
 
 public class GameplayScreen extends AbstractScreen
 {
 	private Ball ball;
+	private Button ballButton;
 
 	public GameplayScreen(JuggleMasterGame game)
 	{
@@ -16,6 +21,26 @@ public class GameplayScreen extends AbstractScreen
 	protected void init()
 	{
 		initBall();
+		initBallButton();
+	}
+
+	private void initBallButton()
+	{
+		ballButton = new Button(new Button.ButtonStyle());
+		ballButton.setWidth(ball.getWidth());
+		ballButton.setHeight(ball.getHeight());
+		ballButton.setX(ball.getX());
+		ballButton.setY(ball.getY());
+		ballButton.setDebug(true);
+		stage.addActor(ballButton);
+		ballButton.addListener(new ClickListener(){
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
+			{
+
+				return super.touchDown(event, x, y, pointer, button);
+			}
+		});
 	}
 
 	private void initBall()
